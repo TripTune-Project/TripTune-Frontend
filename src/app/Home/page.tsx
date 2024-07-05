abd31fe3fd82794229bd4d5aa3e380e97b03f567
hyo814 <ggamjige8888@naver.com>
2024-07-05 20:17:57 +0900

    [24-07-05] 로그아웃 기본 틀

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,3 +1,31 @@
-export default function HomePage() {
-  return <h1>홈 화면</h1>;
-}
+"use client";
+
+import React, { useState } from 'react';
+import LogoutModal from '../../components/Logout/LogoutModal';
+import { logout } from '../../api/logout';
+
+const Home: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+  
+  const handleLogout = async () => {
+    closeModal();
+    await logout();
+  };
+  
+  return (
+    <div>
+      메인 페이지 - 홈
+      <button onClick={openModal}>로그아웃</button>
+      <LogoutModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onConfirm={handleLogout}
+      />
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Logout/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomePage() {
-  return <h1>페이지</h1>;
-}
