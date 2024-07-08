fad591d034705c0025c95ae09d3d6cde619226cf
hyo814 <ggamjige8888@naver.com>
2024-07-08 09:56:36 +0900

    [24-07-08] 로그아웃 구현

--- src/api/logout.ts
+++ src/api/logout.ts
@@ -1,4 +1,16 @@
+import Cookies from 'js-cookie';
+
 export const logout = async () => {
-	await fetch('/api/members/logout', {method: 'PATCH'});
-	window.location.href = '/';
+	const accessToken = Cookies.get('trip-tune_at');
+	if (accessToken) {
+		await fetch('/api/members/logout', {
+			method: 'PATCH',
+			headers: {
+				'Authorization': `Bearer ${accessToken}`
+			}
+		});
+		Cookies.remove('trip-tune_at');
+		Cookies.remove('trip-tune_rt');
+		window.location.href = '/';
+	}
 };
