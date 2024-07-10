9d84ab9180d033d925d376fc9e47497d4c7a2de9
hyo814 <ggamjige8888@naver.com>
2024-07-11 00:52:23 +0900

    [24-07-11] 이번주 회의 진행

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -8,8 +8,10 @@ import LogoutModal from "@/components/Logout/LogoutModal";
 import { Alert, Snackbar } from "@mui/material";
 import { logoutApi } from "@/api/logoutApi";
 import Button from '@mui/material/Button';
+import {useRouter} from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
   const [loginTrue, setLoginTrue] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [alertOpen, setAlertOpen] = useState(false);
@@ -22,7 +24,7 @@ const Header = () => {
     closeModal();
     try {
       await logoutApi();
-      // 로그아웃 성공 시, 필요한 추가 작업 수행
+      router.push("/Home")
     } catch (error) {
       setAlertMessage('로그아웃에 실패했습니다. 다시 시도해 주세요.');
       setAlertOpen(true);
