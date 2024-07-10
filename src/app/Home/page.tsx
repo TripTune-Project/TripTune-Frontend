2ef55e653f6ca425cb80a37826cb8785985f6568
hyo814 <ggamjige8888@naver.com>
2024-07-11 01:30:04 +0900

    [24-07-11] 이미지 슬라이더 1차

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import React from 'react';
+import SwiperComponent from '../../components/Home/index';
+import React from "react";
 
 const Home: React.FC = () => {
+  const images = [
+    'https://picsum.photos/seed/picsum/1000/300',
+    'https://picsum.photos/seed/picsum/1000/301',
+    'https://picsum.photos/seed/picsum/1000/302',
+    'https://picsum.photos/seed/picsum/1000/303',
+    'https://picsum.photos/seed/picsum/1000/304',
+  ];
   
   return (
     <div>
       메인 페이지 - 홈
+      <SwiperComponent images={images} />
     </div>
   );
 };
