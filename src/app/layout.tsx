6009061879a887a9786a7aa1af7e53212b6cfc3b
hyo814 <ggamjige8888@naver.com>
2024-09-06 21:00:59 +0900

    [24-09-06] 라이트 하우스 개선 02

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ import logoImg from '../../public/white_Logo.png';
 import '../styles/global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Google Analytics Measurement ID 가져오기
 const GA4_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID;
 
 if (GA4_MEASUREMENT_ID) {
@@ -50,7 +49,7 @@ const Layout = ({ children }: LayoutProps) => {
       <title>TripTune</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
