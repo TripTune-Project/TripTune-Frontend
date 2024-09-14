5bfd01a0d849642183c851f8eea44614e011018c
hyo814 <ggamjige8888@naver.com>
2024-09-14 10:13:14 +0900

    [2024-09-14] middleware

--- next.config.js
+++ next.config.js
@@ -24,10 +24,22 @@ const nextConfig = {
   },
   // todo : 이걸 써야할지 말지를 모르겠음
   async rewrites() {
+    // 리라이트 규칙 설정
     return [
       {
         source: '/api/:path*',
         destination: 'http://13.209.177.247:8080/api/:path*',
+      },
+      {
+        source: '/api/:path*',
+        destination: '/404',
+        has: [
+          {
+            type: 'query',
+            key: 'path',
+            value: '^(?!.*\\/api\\/).*$',
+          },
+        ],
       },
     ];
   },
