7108a52e7365020ce087568164754665b7874749
hyo814 <ggamjige8888@naver.com>
2024-10-03 01:08:23 +0900

    일정 만들기 모달 창 진행

--- src/app/Schedule/page.tsx
+++ src/app/Schedule/page.tsx
@@ -1,3 +1,33 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import ScheduleModal from '../../components/Schedule/ScheduleModal';
+
 export default function Schedule() {
-  return <h1>스케줄 작성</h1>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  
+  const handleConfirm = (startDate: Date | null, endDate: Date | null) => {
+    console.log('시작 날짜:', startDate);
+    console.log('종료 날짜:', endDate);
+    handleCloseModal();
+  };
+  
+  useEffect(() => {
+    setIsModalOpen(true);
+  }, []);
+  
+  return (
+    <div>
+      <h1>스케줄 작성</h1>
+      <ScheduleModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onConfirm={handleConfirm}
+      />
+    </div>
+  );
 }
