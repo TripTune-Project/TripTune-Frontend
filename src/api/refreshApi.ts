4491b290567a8bdecc7f3af87599f20711fce66c
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2024-10-17 18:05:48 +0900

    Schedule bug (#117)
    Schedule bug (#117)

--- src/api/refreshApi.ts
+++ src/api/refreshApi.ts
@@ -1,10 +1,8 @@
 import { post } from './api';
 import Cookies from 'js-cookie';
-import saveLocalContent from '@/utils/saveLocalContent';
 import Router from 'next/router';
 
 export const refreshApi = async (): Promise<string> => {
-  const { setEncryptedCookie } = saveLocalContent();
   const refreshToken = Cookies.get('trip-tune_rt');
 
   if (!refreshToken) throw new Error('리프레시 토큰을 사용할 수 없습니다.');
@@ -16,10 +14,9 @@ export const refreshApi = async (): Promise<string> => {
         refreshToken,
       }
     );
-
-    const newAccessToken = response.data.accessToken;
-    setEncryptedCookie('trip-tune_at', newAccessToken, 5 / (24 * 60));
-    return newAccessToken;
+    
+    return response.data.accessToken;
+    
   } catch (error) {
     console.error('액세스 토큰 갱신 에러:', error);
 
