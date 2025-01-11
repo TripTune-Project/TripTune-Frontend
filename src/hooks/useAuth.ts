65931ec939101844b1eea1aabac55e4d8ceaf67a
hyo814 <ggamjige8888@naver.com>
2025-01-11 22:55:18 +0900

    로그인 모달 안에서 만료 비교 함수 삭제

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,47 +4,42 @@ import saveLocalContent from '@/utils/saveLocalContent';
 
 const useAuth = () => {
   const isRefreshing = useRef(false);
-
-  const isTokenExpired = (token: string) => {
-    try {
-      const [, payload] = token.split('.');
-      const decoded = JSON.parse(atob(payload));
-      return decoded.exp * 1000 < Date.now();
-    } catch (error) {
-      return true;
-    }
-  };
-
+  
+  // 로그인 모달을 표시하는 함수
   const showLoginModal = () => {
     const event = new CustomEvent('showLoginModal');
     window.dispatchEvent(event);
   };
-
+  
+  // 사용자 인증 상태를 확인하는 함수
   const checkAuthStatus = async () => {
     const { getDecryptedCookie } = saveLocalContent();
     const accessToken = getDecryptedCookie('trip-tune_at');
     const refreshToken = getDecryptedCookie('trip-tune_rt');
-
-    if (!accessToken || isTokenExpired(accessToken)) {
+    
+    // 액세스 토큰이 없는 경우 처리
+    if (!accessToken) {
       if (refreshToken && !isRefreshing.current) {
         isRefreshing.current = true;
         try {
-          await refreshApi();
+          await refreshApi(); // 토큰 새로고침 API 호출
         } catch {
-          showLoginModal();
+          showLoginModal(); // 새로고침 실패 시 로그인 모달 표시
         } finally {
           isRefreshing.current = false;
         }
       } else if (!refreshToken) {
-        showLoginModal();
+        showLoginModal(); // 리프레시 토큰도 없는 경우 로그인 모달 표시
       }
     }
   };
-
+  
+  // 컴포넌트가 마운트될 때 인증 상태 확인
   useEffect(() => {
     checkAuthStatus();
   }, []);
-
+  
+  // 인증 상태 확인 함수를 반환
   return { checkAuthStatus };
 };
 
