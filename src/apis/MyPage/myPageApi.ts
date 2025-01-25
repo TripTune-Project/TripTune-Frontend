98a81e4ad5925998738a5cb8a3fa5dfbababc74f
hyo814 <ggamjige8888@naver.com>
2025-01-25 23:10:56 +0900

    마이페이지 API 관련 URL 수정하기

--- src/apis/MyPage/myPageApi.ts
+++ src/apis/MyPage/myPageApi.ts
@@ -4,13 +4,14 @@ import { BookmarkResponse } from '@/types/myPage';
 // 마이페이지 - 프로필 관리
 // 회원정보 조회
 export const getMyPage = async () => {
-  const url = '/api/mypage/profiles';
+  const url = '/api/members/profile';
 
   try {
     return await get<{
       success: boolean;
       data: {
         userId: string;
+        email: string;
         nickname: string;
         createdAt: string;
         profileImage: string;
@@ -28,7 +29,7 @@ export const updateMyPage = async (
   nickname: string,
   profileImage: File | null
 ): Promise<{ success: boolean; message: string }> => {
-  const url = `/api/mypage/profiles`;
+  const url = `/api/members/profile`;
 
   const formData = new FormData();
   formData.append('nickname', nickname);
@@ -51,15 +52,15 @@ export const updateMyPage = async (
 
 // 마이페이지 - 계정 관리
 // 이메일 인증, 검증은 Verify 파일에서 관리
+// TODO : 사용자 계정 정보 조회 -> 확인중!
 
-// 비밀 번호 변경
 // 비밀번호 변경 (PATCH)
 export const changePassword = async (
   nowPassword: string,
   newPassword: string,
   rePassword: string
 ) => {
-  const url = '/api/mypage/account/change-password';
+  const url = '/api/members/account/change-password';
 
   try {
     return await patch<{ success: boolean; message: string }>(
@@ -75,7 +76,7 @@ export const changePassword = async (
 
 // 회원 탈퇴 (PATCH)
 export const deactivateAccount = async (password: string) => {
-  const url = '/api/mypage/account/deactivate';
+  const url = '/api/members/deactivate';
 
   try {
     return await patch<{ success: boolean; message: string }>(
@@ -91,7 +92,6 @@ export const deactivateAccount = async (password: string) => {
 
 // 마이페이지 - 북마크
 // 북마크 조회 (GET)
-// orderBy TODO : 이해 못함
 export const getBookmarks = async (
   page: number = 1,
   orderBy: 'newest' | 'oldest' | 'name' = 'newest'
@@ -100,7 +100,7 @@ export const getBookmarks = async (
   data?: BookmarkResponse;
   message: string;
 }> => {
-  const url = `/api/mypage/bookmarks?page=${page}&orderBy=${orderBy}`;
+  const url = `/api/members/bookmarks?page=${page}&orderBy=${orderBy}`;
 
   try {
     return await get<{
