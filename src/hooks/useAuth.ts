52a90f147a257dd0c8c08056a7fd2e07bb67e06f
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2025-02-03 22:55:47 +0900

    auth 에러 확인 중 (#190)

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -31,7 +31,7 @@ const useAuth = () => {
   useEffect(() => {
     // 최초 인증 체크
     checkAuth();
-    // 이후 60초마다 주기적으로 체크 (필요에 따라 간격 조정)
+    // 이후 60초마다 주기적으로 체크
     const intervalId = setInterval(checkAuth, 60000);
     return () => clearInterval(intervalId);
   }, [getDecryptedCookie]);
