bf6536c9f0ca584be15d4d3ef2d6fec7bbe2cc31
hyo814 <ggamjige8888@naver.com>
2025-04-27 11:50:56 +0900

    소셜 로그인 동작 확인 : 콘솔 추가

--- src/app/oauth/callback/OAuthCallbackClient.tsx
+++ src/app/oauth/callback/OAuthCallbackClient.tsx
@@ -15,18 +15,34 @@ export default function OAuthCallbackClient() {
   useEffect(() => {
     const handleOAuthCallback = async () => {
       try {
+        // URL 파라미터 전체 확인
+        console.log('전체 URL 파라미터:', Object.fromEntries(searchParams.entries()));
+        
         const accessToken = searchParams.get('accessToken');
         const refreshToken = searchParams.get('refreshToken');
         const nickname = searchParams.get('nickname');
 
+        console.log('받은 토큰과 닉네임:', {
+          accessToken: accessToken ? '있음' : '없음',
+          refreshToken: refreshToken ? '있음' : '없음',
+          nickname: nickname || '없음'
+        });
+
         if (!accessToken || !refreshToken || !nickname) {
+          console.error('필수 파라미터 누락:', {
+            accessToken: !accessToken,
+            refreshToken: !refreshToken,
+            nickname: !nickname
+          });
           throw new Error('소셜 로그인에 실패했습니다.');
         }
 
         // 토큰과 닉네임을 쿠키에 저장
         setEncryptedCookie('trip-tune_at', accessToken, 5 / (24 * 60));
-        setEncryptedCookie('trip-tune_rt', refreshToken, 7); // 리프레시 토큰 저장
+        setEncryptedCookie('trip-tune_rt', refreshToken, 7);
         setEncryptedCookie('nickname', nickname, 7);
+
+        console.log('쿠키 저장 완료');
 
         // 인증 상태 업데이트
         updateAuthStatus(true);
