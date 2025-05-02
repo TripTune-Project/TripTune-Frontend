9223511c7e2a7e6401deac880745685de3afe20c
hyo814 <ggamjige8888@naver.com>
2025-05-02 21:20:37 +0900

    리프레시 토큰 업데이트

--- src/apis/Login/refreshApi.ts
+++ src/apis/Login/refreshApi.ts
@@ -1,15 +1,10 @@
 import { post } from '../api';
 import saveLocalContent from '@/utils/saveLocalContent';
-import Cookies from 'js-cookie';
 
 export const refreshApi = async (): Promise<{
   accessToken: string;
   nickname: string;
 }> => {
-  const refreshToken = Cookies.get('refreshToken');
-  if (!refreshToken) {
-    throw new Error('리프레시 토큰이 없습니다.');
-  }
   try {
     const response = await post<{
       data: {
@@ -24,13 +19,13 @@ export const refreshApi = async (): Promise<{
         requiresAuth: false,
       }
     );
-
+    
     const { accessToken, nickname } = response.data;
-
+    
     const { setEncryptedCookie } = saveLocalContent();
     setEncryptedCookie('accessToken', accessToken);
     setEncryptedCookie('nickname', nickname);
-
+    
     return { accessToken, nickname };
   } catch (error: unknown) {
     console.error('리프레시 중 오류 발생:', error);
