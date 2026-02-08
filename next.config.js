0e86582748f202c134189a4bd46ca76dc7d491f6
hjlim <hjlim@gitsn.com>
2026-02-08 20:56:01 +0900

    경로 참조 방식을 절대 경로로 변경 및 Webpack alias 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true,
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
   images: {
     remotePatterns: [
